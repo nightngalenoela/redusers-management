61c1118e1f6a9352e16df51fd06eb82a0a71bca4
flo <nightngalenoela@gmail.com>
2021-06-29 11:49:32 +0300

    search user by id

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
+//Create Redis Client
+let client = redis.createClient();
+
+client.on('connect', function () {
+    console.log('Connected to Redis...')
+})
+
 // Set Port
 const port = 3000;
 
@@ -12,7 +19,7 @@ const port = 3000;
 const app = express();
 
 // View Engine 
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // body-parser
@@ -20,15 +27,34 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded());
 
 // parse application/json
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 //methodOverride
 app.use(methodOverride('_method'));
 
-app.get('/', function(req, res, next){
+// Search Page
+app.get('/', function (req, res, next) {
     res.render('searchusers');
 });
 
-app.listen(port, function(){
-    console.log('Server started on port '+port);
+//Search Processing
+app.post('/user/search', function (req, res, next) {
+    let id = req.body.id;
+
+    client.hgetall(id, function (err, obj) {
+        if (!obj) {
+            res.render('searchusers', {
+                error: 'User does not exist'
+            })
+        } else {
+            obj.id = id;
+            res.render('details', {
+                user: obj
+            });
+        }
+    });
+});
+
+app.listen(port, function () {
+    console.log('Server started on port ' + port);
 });
